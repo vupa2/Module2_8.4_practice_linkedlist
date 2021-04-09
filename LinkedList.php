94b24c2510f2011a05634119bb48e86cd2693192
vupa2 <anhvu.pa96@gmail.com>
2021-04-09 15:30:11 +0700

    Add files via upload

---
+++ LinkedList.php
@@ -0,0 +1,135 @@
+<?php
+
+require_once 'Node.php';
+
+class LinkedList
+{
+  public $count;
+  public $firstNode;
+  public $lastNode;
+
+  public function __construct()
+  {
+    $this->count = 0;
+    $this->firstNode = null;
+    $this->lastNode = null;
+  }
+
+  public function printList()
+  {
+    $arr = [];
+    for ($current = $this->firstNode; $current != null; $current = $current->next) {
+      array_push($arr, $current->readNode());
+    }
+    return $arr;
+  }
+
+  public function size()
+  {
+    return $this->count;
+  }
+
+  public function add($index, $data)
+  {
+    if ($index < 0 || $index > $this->count) {
+      echo "Invalid index";
+    } else if ($index == 0) {
+      $this->addFirst($data);
+    } else if ($index == $this->count) {
+      $this->addLast($data);
+    } else {
+      $link = new Node($data);
+      $current = $this->firstNode;
+
+      for ($i = 0; $i < $index - 1; $i++) {
+        $current = $current->next;
+      }
+      $link->next = $current->next;
+      $current->next = $link;
+      $this->count++;
+    }
+  }
+
+  public function addFirst($data)
+  {
+    $link = new Node($data);
+    $link->next = $this->firstNode;
+    $this->firstNode = $link;
+
+    if ($this->lastNode == null) {
+      $this->lastNode = $link;
+    }
+
+    $this->count++;
+  }
+
+  public function addLast($data)
+  {
+    $link = new Node($data);
+    $link->next = null;
+
+   if ($this->count == 0) {
+     $this->firstNode = $this->lastNode = $link;
+   } else {
+     $this->lastNode->next = $link;
+     $this->lastNode = $link;
+   }
+
+   $this->count++;
+  }
+
+  public function remove($index)
+  {
+    if ($index < 0 || $index >= $this->count) {
+      echo "Invalid index";
+    } else if ($index == 0) {
+    }
+  }
+
+  public function removeFirst()
+  {
+    if ($this->count == 0) {
+      echo "Empty Linked List";
+    } else if ($this->count == 1) {
+      $this->firstNode = $this->lastNode = null;
+      $this->count = 0;
+    } else {
+      $this->firstNode = $this->firstNode->next;
+      $this->count--;
+    }
+  }
+
+  public function removeLast()
+  {
+    if ($this->size == 0) {
+      echo "Empty Linked List";
+    } else if ($this->count == 1) {
+      $this->firstNode = $this->lastNode = null;
+      $this->count = 0;
+    } else {
+      $current = $this->firstNode;
+      for ($i = 0; $i < $this->count - 2; $i++) {
+        $current = $current->next;
+      }
+
+      $this->lastNode = $current;
+      $this->lastNode->next = null;
+      $this->count--;
+    }
+  }
+
+  public function get($index)
+  {
+    if ($this->count == 0) {
+      echo "Empty Linked List";
+    } else if ($index < 0 || $index >= $this->count) {
+      echo "Invalid index";
+    } else {
+      $current = $this->firstNode;
+      for ($i = 0; $i < $index; $i++) {
+        $current = $current->next;
+      }
+      return $current->readNode();
+    }
+  }
+}
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'LinkedList.php';
+
+$data1 = new stdClass();
+$data1->id = 1;
+$data1->name = "BMW";
+$data1->colot = "blue";
+
+$data2 = new stdClass();
+$data2->id = 2;
+$data2->name = "TOY";
+$data2->colot = "red";
+
+$data3 = new stdClass();
+$data3->id = 3;
+$data3->name = "VIN";
+$data3->colot = "green";
+
+$data4 = new stdClass();
+$data4->id = 4;
+$data4->name = "ROL";
+$data4->colot = "yellow";
+
+$data5 = new stdClass();
+$data5->id = 5;
+$data5->name = "YAM";
+$data5->colot = "brown";
+
+$linkedList = new LinkedList();
+$linkedList->addFirst($data1);
+$linkedList->addLast($data2);
+$linkedList->addLast($data3);
+
+echo "<pre>";
+var_dump($linkedList->printList());
